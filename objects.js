90ca0361518802e0f5bdae475a57c0afaeccdcfb
Heather Zurek <Heather.Zurek@oiaglobal.com>
2019-08-30 14:28:08 -0700

    uses the math object

--- objects.js
+++ objects.js
@@ -49,3 +49,23 @@ user.logout();
 
 // console.log(typeof user);
 
+//MATH OBJECT
+
+console.log(Math.PI)
+console.log(Math.E)
+
+const area = 7.5;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+// geneterate random numbers
+
+const random = Math.random();
+
+
+console.log(random);
+console.log(Math.round(random* 100));
+
