4c47368a06b761f78d986a22655444474b7de4a7
Heather Zurek <heather.zurek@gmail.com>
2019-08-31 22:34:16 -0700

    event listeners in javascript

--- chaptersix.js
+++ chaptersix.js
@@ -73,20 +73,61 @@
 // console.log(content.classList);
 // content.classList.add('success');
 
-const paras = document.querySelectorAll('p');
+// const paras = document.querySelectorAll('p');
 
-paras.forEach(p => {
-    console.log(p.textContent);
-    if(p.textContent.includes('error')){
-        p.classList.add('error');
-    }
-    if(p.textContent.includes('success')){
-        p.classList.add('success');
-    }
+// paras.forEach(p => {
+//     console.log(p.textContent);
+//     if(p.textContent.includes('error')){
+//         p.classList.add('error');
+//     }
+//     if(p.textContent.includes('success')){
+//         p.classList.add('success');
+//     }
+// });
+
+// const title = document.querySelector('.title');
+
+// title.classList.toggle('test');
+// title.classList.toggle('test');
+
+//node tree is visual representation of dom
+//elements on same level are siblings
+//parent such as div, with p would be their children
+
+// const article = document.querySelector('article');
+
+// console.log(article.children);
+// Array.from(article.children).forEach((child) => {
+//     child.classList.add('article-element');
+// });
+
+// const title = document.querySelector('h2');
+// //node relations
+// console.log(title.parentElement);
+// console.log(title.parentElement.parentElement);
+// console.log(title.nextElementSibling);
+// console.log(title.previousElementSibling);
+// //chaining to traverse the DOM
+// console.log(title.nextElementSibling.parentElement.children);
+
+//query the dom
+//event listener
+//callback
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    console.log('you clicked me');
 });
 
-const title = document.querySelector('.title');
+const items = document.querySelectorAll('li');
 
-title.classList.toggle('test');
-title.classList.toggle('test');
-
+items.forEach(item => {
+    item.addEventListener('click', e => {
+        // console.log('item clicked');
+        // console.log(e);
+        // console.log(e.target);
+        // console.log(item);
+        e.target.style.textDecoration = 'line-through';
+    });
+});
