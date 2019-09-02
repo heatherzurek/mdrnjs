130dd8009970110719540b6dad213373c90946a7
Heather Zurek <heather.zurek@gmail.com>
2019-09-01 22:34:25 -0700

    finished chapter 7

--- chapterseven.js
+++ chapterseven.js
@@ -4,6 +4,8 @@
 const form = document.querySelector('.signup-form');
 // const username = document.querySelector('#username');
 const feedback = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -11,8 +13,6 @@ form.addEventListener('submit', e => {
     //console.log(username.value);
     //validation
     const username = form.username.value;
-    const usernamePattern = /^[a-zA-Z]{6,12}$/;
-
     if(usernamePattern.test(username)){
         //feedback good info
         feedback.textContent = 'That username is valid.';
@@ -24,8 +24,6 @@ form.addEventListener('submit', e => {
 
 //RegEx
 //Matching patterns
-const username = 'heather';
-const pattern = /^[a-z]{6,}$/;
 
 // let result = pattern.test(username);
 // if(result){
@@ -33,8 +31,21 @@ const pattern = /^[a-z]{6,}$/;
 // } else {
 //     console.log('regex test failed');
 // }
-
-let result = username.search(pattern);
-console.log(result);
+// let result = username.search(pattern);
+// console.log(result);
 //returns the position of the match
 
+//keyup event 
+//live feedback
+form.username.addEventListener('keyup', e => {
+    console.log(e);
+    // console.log(e.target.value, form.username.value);
+    if(usernamePattern.test(e.target.value)){
+        form.username.setAttribute('class', 'success');
+        console.log('passed');
+    } else {
+        form.username.setAttribute('class', 'error');
+
+        console.log('failed');
+    }
+});
