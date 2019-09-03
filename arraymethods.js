218636b09c7e0ccac781eef47d7fb49e69792e06
Heather Zurek <Heather.Zurek@oiaglobal.com>
2019-09-03 08:50:54 -0700

    map method on arrays

--- arraymethods.js
+++ arraymethods.js
@@ -43,3 +43,33 @@ const vistedParks = parks.filter((park) => {
 console.log(vistedParks);
 
 
+//MAP METHOD
+// takes an array and maps it into a new array
+// iterates an array and returns an updated value and pushes it into a new array
+//cycles through an array
+//creates new array based on that array
+const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+
+const salePrices = prices.map(price => {
+    return price / 2;
+});
+
+console.log(salePrices);
+
+const products = [
+    {name: 'gold star', price: 20},
+    {name: 'mushroom', price: 40},
+    {name: 'green shells', price: 30},
+    {name: 'banana skin', price: 10},
+    {name: 'red shells', price: 50},
+];
+
+const saleProducts = products.map(product => {
+    if(product.price > 30){
+        return {name: product.name, price: product.price / 2};
+    } else {
+        return product;
+    }
+});
+
+console.log(saleProducts);
